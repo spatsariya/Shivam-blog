4f8d95a92c78e3ffa2a2bb1c45624edd6ffeea06
Shivam Patsariya <s.patsariya@gmail.com>
2025-04-20 03:17:43 -0400

    Trying the refresh rate

--- install.php
+++ install.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-
+//added by SHivam for testy
 // Enable error reporting for debugging during installation
 //error_reporting(E_ALL);
 //ini_set('display_errors', 1);
